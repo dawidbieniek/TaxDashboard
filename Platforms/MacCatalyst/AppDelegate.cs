e12b91c53d617779963f07d4002c6e2466b98a3f
Dawid Bieniek <dawid_b01@wp.pl>
2024-12-19 15:57:47 +0100

    Setup project

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TaxDashboard
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
