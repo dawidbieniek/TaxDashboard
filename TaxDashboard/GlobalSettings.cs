1b5709d9beae5cf55ec94c4a4bbd495d890f4503
Dawid Bieniek <dawid_b01@wp.pl>
2025-01-30 00:41:18 +0100

    Add full selection to input fields on click

--- TaxDashboard/GlobalSettings.cs
+++ TaxDashboard/GlobalSettings.cs
@@ -4,6 +4,6 @@ namespace TaxDashboard;
 
 public static class GlobalSettings
 {
-    public const NumberStyles CurrencyNumberStyles = NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign | NumberStyles.AllowCurrencySymbol;
+    public const NumberStyles CurrencyNumberStyles = NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign | NumberStyles.AllowCurrencySymbol | NumberStyles.Currency | NumberStyles.AllowThousands;
     public static readonly CultureInfo CurrencyCulture = CultureInfo.CreateSpecificCulture("pl-PL");
 }
