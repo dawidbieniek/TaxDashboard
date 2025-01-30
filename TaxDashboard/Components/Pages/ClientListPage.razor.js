11aa9b4b423d2c0492f1e46d23801321ff595d68
Dawid Bieniek <dawid_b01@wp.pl>
2025-01-31 00:44:27 +0100

    Add button for client deletion in table

---
+++ TaxDashboard/Components/Pages/ClientListPage.razor.js
@@ -0,0 +1,6 @@
+﻿export function addEventListeners(dotnetRef) {
+    const modal = document.getElementById('deletingClientModal');
+    modal.addEventListener('hidden.bs.modal', e => {
+        dotnetRef.invokeMethodAsync('OnModalHidden');
+    });
+}
